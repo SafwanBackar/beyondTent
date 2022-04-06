ca6c762ebbcde0bb7aa052984746abb84eb11cb0
Safwan Backar <sbackar29@gmail.com>
2022-04-06 21:07:38 +0530

    comments edit and delete

--- v10/routes/comment.js
+++ v10/routes/comment.js
@@ -44,6 +44,41 @@ router.post('/', isLoggedIn, function (req, res) {
     })
 })
 
+// Comment Edit
+router.get('/:comment_id/edit', function (req, res) {
+    Comment.findById(req.params.comment_id, function (err, foundComment) {
+        if (err) {
+            res.redirect('back')
+        } else {
+            res.render('comments/edit', { campground_id: req.params.id, comment: foundComment })
+        }
+    })
+})
+
+// Comment Update
+router.put('/:comment_id', function (req, res) {
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function (err, updatedComment) {
+        if (err) {
+            res.redirect('back')
+        } else {
+            res.redirect('/campgrounds/' + req.params.id)
+        }
+    })
+})
+
+// Comment Delete
+
+router.delete('/:comment_id', function (req, res) {
+    Comment.findByIdAndRemove(req.params.comment_id, function (err) {
+        if (err) {
+            res.redirect('back')
+        } else {
+            res.redirect('/campgrounds/' + req.params.id)
+        }
+    })
+})
+
+// middleware
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
         return next();
